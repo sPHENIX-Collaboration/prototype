2a7682740e2513084378114e05daa38ce4a8e7e0
Yasser Corrales Morales <yasser.corrales.morales@cern.ch>
2019-07-16 16:35:58 +0200

    more identation fixes

--- macros/MvtxPrototype2/Draw_hitmap.C
+++ macros/MvtxPrototype2/Draw_hitmap.C
@@ -1,49 +1,42 @@
+#include "Style.h"
 
 void Draw_hitmap(int runnum=732, const bool bSAVE=false){
 
-	gStyle->SetOptStat(0);
+  gStyle->SetOptStat(0);
 
-	const int nstave = 4;
-	const int nchip = 9;
+  const int nstave = 4;
+  const int nchip = 9;
 
-	TFile *infile = new TFile(Form("MvtxPrototype2Eval-%08d-0200.root",runnum),"read");
+  TFile *infile = new TFile(Form("MvtxPrototype2Eval-%08d-0200.root",runnum),"read");
+  assert(infile);
 
-	TH2D *h2d_hit[nstave][nchip];
+  TH2D *h2d_hit[nstave];
 
-	for (int is=0; is<nstave; is++){
-		for (int ic=0; ic<nchip; ic++){
+  for (int is=0; is<nstave; is++){
+    h2d_hit[is] = (TH2D*)infile->Get(Form("h2d_hit_stave%d",is));
+    assert(h2d_hit[is])
+  }
 
-			h2d_hit[is][ic] = (TH2D*)infile->Get(Form("h2d_hit_stave%d_chip%d",is,ic));
+  TCanvas *c1 = new TCanvas("c1", "c1", 300*nchip, 150*nstave);
+  c1->Divide(1,nstave);
 
-		}
-	}
+  for (int istave=0; istave<nstave; istave++){
+    c1->cd(istave + 1);
+    SetPadStyle();
+    gPad->SetRightMargin(0.01);
+    gPad->SetTopMargin(0.05);
 
-	TCanvas *c1 = new TCanvas("c1","c1",300*nchip,150*nstave);
-	c1->Divide(nchip, nstave);
+    htmp = (TH1D*)h2d_hit[istave];
+    SetHistoStyle("COL","ROW","", 0.07, 0.06);
+    h2d_hit[istave]->Draw("box");
 
-	for (int istave=0; istave<nstave; istave++){
-		for (int ichip=0; ichip<nchip; ichip++){
+    TLatex *tex = new TLatex(100,400,Form("Stave%d", istave));
+    tex->SetTextSize(0.07);
+    tex->Draw();
+  }
 
-			c1->cd(nchip*istave + ichip + 1);
-			SetPadStyle();
-			gPad->SetRightMargin(0.01);
-			gPad->SetTopMargin(0.05);
-
-			htmp = (TH1D*)h2d_hit[istave][ichip];
-			SetHistoStyle("COL","ROW","",0.07,0.06);
-			h2d_hit[istave][ichip]->Draw("box");
-
-			TLatex *tex = new TLatex(100,400,Form("Stave%d, Chip%d",istave,ichip));
-			tex->SetTextSize(0.07);
-			tex->Draw();
-
-		}
-	}
-
-	if ( bSAVE ){
-		c1->cd();
-		c1->SaveAs(Form("./20190503/hitmap_%08d.pdf",runnum));
-	}
-
-
+  if (bSAVE){
+    c1->cd();
+    c1->SaveAs(Form("./20190503/hitmap_%08d.pdf", runnum));
+  }
 }

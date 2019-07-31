bae83e687a0253cc7c6cf9376191d018249945bb
Yasser Corrales Morales <yasser.corrales.morales@cern.ch>
2019-07-31 16:00:08 +0200

    Add simple tracking

--- macros/MvtxPrototype2/Fun4All_TestBeam.C
+++ macros/MvtxPrototype2/Fun4All_TestBeam.C
@@ -11,10 +11,13 @@
 #include <mvtxprototype2/MvtxPrototype2UnpackPRDF.h>
 #include <mvtxprototype2/MvtxPrototype2Clusterizer.h>
 #include <mvtxprototype2/MvtxPrototype2Align.h>
-#include <mvtxprototype2/MvtxPrototype2Eval.h>
+#include <anamvtxprototype2/AnaMvtxBeamTest2019.h>
+
+#include <TSystem.h>
 
 R__LOAD_LIBRARY(libfun4all.so)
 R__LOAD_LIBRARY(libMvtxPrototype2.so)
+R__LOAD_LIBRARY(libAnaMvtxBeamTest2019.so)
 #endif
 
 #include <string>
@@ -23,19 +26,17 @@ using namespace std;
 
 void Fun4All_TestBeam(
 		int nEvents = 10,
-		const char *input_file = "calib-00000648-0000.prdf",
-		const char *output_file = "DST-calib-00000648-0000.root",
-    bool do_align = true
-		)
+		const string &input_file = "calib-00000648-0000.prdf")
 {
   gSystem->Load("libfun4all");
   gSystem->Load("libMvtxPrototype2.so");
+  gSystem->Load("libAnaMvtxBeamTest2019.so");
 
   Fun4AllServer *se = Fun4AllServer::instance();
   se->Verbosity(Fun4AllServer::VERBOSITY_SOME);
 
-	char ifile[strlen(input_file)+1]; // + 1 for the \0 which marks the end of string
-	strcpy(ifile,input_file);
+	char ifile[input_file.length()+1]; // + 1 for the \0 which marks the end of string
+	strcpy(ifile, input_file.c_str());
 	strtok(ifile,"-");
 	int runnumber = atoi(strtok(0,"-"));
 	int segnumber = atoi(strtok(strtok(0,"-"),"."));
@@ -66,27 +67,70 @@ void Fun4All_TestBeam(
 	clus->Verbosity(0);
 	se->registerSubsystem(clus);
 
+  bool do_align = true;
   if ( do_align )
   {
     MvtxPrototype2Align *align = new MvtxPrototype2Align("MvtxAlign");
     //align->SetAlignmentParFileDir("beamcenter/");
     align->SetAlignmentParFileDir("./");
     align->SetAlignmentParFileName("beamcenter_mean_0848_0849_0851.txt");
-    align->PrintAlignmentPars();
+    //align->PrintAlignmentPars();
     align->Verbosity(0);
     se->registerSubsystem(align);
   }
 
-	MvtxPrototype2Eval *qa = new MvtxPrototype2Eval();
-	qa->set_filename(Form("MvtxPrototype2Eval-%08d-%04d.root",runnumber,segnumber+200));
-	se->registerSubsystem(qa);
+  bool do_eval  = true;
+  bool do_standalone_tracking = do_eval && true;
+  bool do_ghost_rej = do_standalone_tracking && true;
+  if ( do_eval )
+  {
+
+    AnaMvtxBeamTest2019 *eval = new AnaMvtxBeamTest2019();
+	  eval->set_out_filename(Form("MvtxBT2019Eval-%08d-%04d.root",
+                           runnumber,segnumber+200));
+    eval->set_do_tracking(do_standalone_tracking);
+    eval->getStandaloneTracking()->SetGhostRejection(do_ghost_rej);
+    eval->getStandaloneTracking()->Verbosity(0);
+    eval->Verbosity(0);
+	  se->registerSubsystem(eval);
+
+  }
+
+/*
+  if ( do_tracking )
+  {
+    //  // Find all clusters associated with each seed track
+    TpcPrototypeGenFitTrkFinder *finder = new TpcPrototypeGenFitTrkFinder();
+    finder->Verbosity(verbosity);
+    finder->set_do_evt_display(eventDisp);
+    finder->set_do_eval(false);
+    finder->set_eval_filename(input_file + "_TpcPrototypeGenFitTrkFinder.root");
+    se->registerSubsystem(finder);
+    ////
+    ////  //
+    //  //------------------------------------------------
+    //  // Fitting of tracks using Kalman Filter
+    //  //------------------------------------------------
+    //
+    TpcPrototypeGenFitTrkFitter *kalman = new TpcPrototypeGenFitTrkFitter();
+    kalman->Verbosity(verbosity);
+    kalman->set_do_evt_display(eventDisp);
+    kalman->set_eval_filename(input_file + "_TpcPrototypeGenFitTrkFitter.root");
+    kalman->set_do_eval(true);
+    se->registerSubsystem(kalman);
+  }
+*/
+
+  bool do_dst = false;
+  if ( do_dst )
+  {
+	  Fun4AllDstOutputManager *out_Manager = new Fun4AllDstOutputManager("DSTOUT",  input_file + "_DST.root" );
+	  se->registerOutputManager(out_Manager);
+  }
 
   Fun4AllInputManager *in = new Fun4AllPrdfInputManager("PRDFin");
   in->fileopen(input_file);
   se->registerInputManager(in);
-
-	//Fun4AllDstOutputManager *out_Manager = new Fun4AllDstOutputManager("DSTOUT",output_file);
-	//se->registerOutputManager(out_Manager);
 
   se->run(nEvents);
 
